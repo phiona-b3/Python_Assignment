29b6f86846ca014fd76ba0ec576b989e8c1f8e04
michelle-njambi <michelle.njambi47@gmail.com>
2025-04-05 11:27:44 +0300

    Update fahrenheit_to_celsius.py

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -1,6 +1,5 @@
 def convert(fahrenheit):
     return 5/9 * (fahrenheit - 32)
-
 fahrenheit = float(input("Enter temperature in Fahrenheit: "))
 celsius = convert(fahrenheit)
 print(f"Temperature in Celsius: {celsius:.2f}")
