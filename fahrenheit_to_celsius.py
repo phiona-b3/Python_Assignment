7b8395997e0a4a44a6e64c602b06921426b243d4
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2025-04-05 11:11:27 +0300

    Update fahrenheit_to_celsius.py

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -1,15 +1,17 @@
+# Function to convert Fahrenheit to Celsius
 def convert(fahrenheit):
-    return 5 / 9 * (fahrenheit - 32)
+   # Convert Fahrenheit to Celsius
+    celsius = 5/9 * (fahrenheit - 32)
+    return celsius
 
-fahrenheit_temp = float(input("Enter temperature in Fahrenheit: "))
-celsius_temp = convert(fahrenheit_temp)
-
-print(f"Celsius: {celsius_temp:.2f}")
-
-if celsius_temp > 20:
-    print("IT'S HOT HERE")
-else:
-    print("IT’S COLD HERE")
-
-
-
+def main():
+    fahrenheit_temp = eval(input("Enter temperature in Fahrenheit: ")) # Accept temperature input from the user in Fahrenheit
+    celsius = convert(fahrenheit)
+    print(f"The temperature in Celsius is: {celsius:.2f}")
+    
+    # Check if it's hot or cold
+    if celsius > 20:
+        print("IT'S HOT HERE")
+    else:
+        print("IT'S COLD HERE")
+main()
